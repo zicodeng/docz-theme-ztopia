e8319796584338801e2f76cec37854338ed93884
Zico Deng <zicodeng@gmail.com>
2019-06-03 19:31:55 -0700

    Delete utils

--- src/components/header/components/Search.tsx
+++ src/components/header/components/Search.tsx
@@ -2,10 +2,29 @@ import React, { useState } from 'react';
 import classNames from 'classnames/bind';
 import { useMenus, useConfig, Link } from 'docz';
 
-import { highlightQuery } from './utils';
 import styles from './Search.css';
 
 const cx = classNames.bind(styles);
+
+const highlightQuery = (query: string, text: string, color: string): string => {
+  const idx = text.toLowerCase().indexOf(query.toLowerCase());
+  if (idx < 0) {
+    if (query.length == 1) {
+      return text;
+    }
+    // Try using the first query character to search and highlight
+    const firstChar = query[0];
+    return highlightQuery(firstChar, text, color);
+  }
+  return idx < 0
+    ? text
+    : text.substring(0, idx) +
+        `<span style="color: ${color}">${text.substring(
+          idx,
+          idx + query.length,
+        )}</span>` +
+        text.substring(idx + query.length);
+};
 
 const Search = () => {
   const [query, setQuery] = useState('');
