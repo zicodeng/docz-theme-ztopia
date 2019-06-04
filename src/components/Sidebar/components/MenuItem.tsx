e6d09243254e91cf991c103b0b9b0b2f377153ed
Zico Deng <zicodeng@gmail.com>
2019-06-03 23:03:38 -0700

    Better sidebar menu item style

--- src/components/Sidebar/components/MenuItem.tsx
+++ src/components/Sidebar/components/MenuItem.tsx
@@ -30,13 +30,23 @@ const MenuItem: FunctionComponent<Props> = memo(({ route, name }) => {
         style={{
           borderRight: `5px solid ${
             isActive
-              ? colors.primary
+              ? theme === 'light'
+                ? colors.primary
+                : colors.primaryLight
               : theme === 'light'
               ? colors.whiteDark
               : colors.blackLight
           }`,
-          color: isActive ? colors.primary : 'inherit',
-          backgroundColor: isActive ? colors.primaryLight : 'inherit',
+          color: isActive
+            ? theme === 'light'
+              ? colors.primary
+              : colors.primaryLight
+            : 'inherit',
+          backgroundColor: isActive
+            ? theme === 'light'
+              ? colors.primaryLight
+              : colors.primary
+            : 'inherit',
           transition: isActive ? 'all 0.3s ease' : null,
         }}
         onMouseEnter={() => {
